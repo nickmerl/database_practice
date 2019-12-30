1c2180df5284773b51a755ee1e0d5d699eda7c2a
nickmerl <f.n.merlino@gmail.com>
2019-12-30 16:07:32 -0500

    write functionality not yet supported, this worked to read in the data

--- google_sheet/googl_sheet.R
+++ google_sheet/googl_sheet.R
@@ -1,19 +1,18 @@
 library(googlesheets)
+
 library(httr)
 
 
-table <- "practice_database"
+table <- 'https://docs.google.com/spreadsheets/d/1KDA8_8gnGC221QaymVdTG_74g10TP9l_9Kj0F65KTVE/edit#gid=0'
 
-saveData <- function(data) {
-  # Grab the Google Sheet
-  sheet <- gs_title(table)
-  # Add the data as a new row
-  gs_add_row(sheet, input = data)
-}
+# saveData <- function(data) {
+#   # Grab the Google Sheet
+#   sheet <- gs_title(table)
+#   # Add the data as a new row
+#   gs_add_row(sheet, input = data)
+# }
 
 loadData <- function() {
   # Grab the Google Sheet
-  sheet <- gs_title(table)
-  # Read the data
-  gs_read_csv(sheet)
+  read_sheet(table, col_names = FALSE)
 }
